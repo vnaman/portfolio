198a8ba988a125443b5b5088449e5d61f9d6c2fd
naman <vnaman555@gmail.com>
2021-08-07 23:02:10 +0530

    Added download resume functionality

--- portfolio/cv/urls.py
+++ portfolio/cv/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home , name = 'cv-home'),
+    path('', views.home, name='cv-home'),
+    path('download/', views.DownloadPDF, name='download_pdf'),
 ]
--- portfolio/cv/views.py
+++ portfolio/cv/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+import os
+from django.conf import settings
+from django.http import HttpResponse, Http404
+from portfolio.settings import BASE_DIR, MEDIA_ROOT
+from django.core.files import File
+from django.http import FileResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'cv/Home.html')
+    return render(request, 'cv/home.html')
+
+def DownloadPDF(self):
+    path_to_file = MEDIA_ROOT + '/NamanVijayvargiya_Resume.pdf'
+    f = open(path_to_file, 'rb')
+    pdfFile = File(f)
+    response = HttpResponse(pdfFile.read(), content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment';
+    return response
