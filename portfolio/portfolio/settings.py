2c5db5b22e069906da5cfd505bee58f51407cc98
naman <vnaman555@gmail.com>
2021-08-08 17:33:58 +0530

    settings

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-t=sp8292dw(gg33%k3i#*mh#)%3coc))zdd*k%*4(62h94uzr7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://namanv-portfolio.herokuapp.com/',
+'localhost',
+'127.0.0.1']
 
 
 # Application definition
